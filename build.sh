5e155a0222c4baba3c044fd0c5d06236bab874d1
Jason Hsu <jhsu802701@jasonhsu.com>
2016-11-25 03:13:03 +0000

    Added build.sh

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo '++++++++++++++++++++++'
+echo 'BEGIN rebuilding _site'
+echo 'rm -rf ./_site'
+rm -rf ./_site
+
+echo '------------------'
+echo 'BEGIN jekyll build'
+jekyll build
+echo 'FINISHED jekyll build'
+echo '---------------------'
+
+echo 'FINISHED rebuilding _site'
+echo '+++++++++++++++++++++++++'
